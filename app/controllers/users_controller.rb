04080fe53f7d84ee31105fde55ff5135a4227506
David Rankin <Rankida@gmail.com>
2012-06-03 11:42:16 +0100

    Finished user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(params[:user])
   	if @user.save
-  		# success
+      flash[:success] = "Welcome to Rankida's Sample App!"
+  		redirect_to @user
   	else
   		render 'new'
   	end
