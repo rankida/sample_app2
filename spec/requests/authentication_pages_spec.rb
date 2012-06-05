55541e2b9aae4386d8d1600ba7b38ac50a3c6a22
David Rankin <Rankida@gmail.com>
2012-06-05 18:42:38 +0100

    Fixed bug where email had to be lower case when signing in.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -14,6 +14,15 @@ describe "Authentication" do
   describe "signin" do
   	before { visit signin_path }
 
+    describe "should not be case sensitive" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        user.email.upcase!
+        sign_in user
+      end
+      it { should have_selector('title', text: user.name) }
+    end
+
   	describe "with invalid information" do
   		before { click_button "Sign in" }
   		it { should have_selector('title', text:'Sign In') }
@@ -33,10 +42,6 @@ describe "Authentication" do
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
-
-      describe "should not be case sensitive" do
-        pending("finish!")
-      end
 
       describe "followed by signout" do
         before { click_link "Sign out" }
