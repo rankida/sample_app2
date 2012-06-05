17f6c6490c259e6a462b17e1556de954dc58906a
David Rankin <Rankida@gmail.com>
2012-06-05 19:23:00 +0100

    Finished 9.2.1, adding basic authorisation to the edit user page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
+
   def show
   	@user = User.find(params[:id])
   end
@@ -25,9 +27,18 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
-      # Handle a successful update
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
     else
       render 'edit'
     end
   end
+
+  private
+
+    def signed_in_user
+      redirect_to signin_path, notice: "Please sign in." unless signed_in?
+    end
+
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -49,4 +49,46 @@ describe "Authentication" do
       end
     end
   end
+
+  describe "authorization" do
+    
+    describe "for non-signed-in user" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "in the Users controller" do
+        
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_selector('title', text: 'Sign In') }
+        end
+
+        describe "submitting to the update action" do
+          before { put user_path(user) }
+          specify { response.should redirect_to(signin_path) }
+        end
+      end
+    end
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
