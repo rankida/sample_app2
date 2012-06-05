6b3121ee5a9ed9af0be0443f851d7596a5b0b93d
David Rankin <Rankida@gmail.com>
2012-06-05 18:31:12 +0100

    Finished 9.1.2. Now able to edit your own details.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -27,12 +27,16 @@ describe "Authentication" do
 
   	describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before { valid_signin(user) }
+      before { sign_in user }
 
       it { should have_selector('title', text: user.name) }
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
+
+      describe "should not be case sensitive" do
+        pending("finish!")
+      end
 
       describe "followed by signout" do
         before { click_link "Sign out" }
