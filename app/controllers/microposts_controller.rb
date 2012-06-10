4c7be66142c04c945edf39a6f1bf142e25d85a5f
David Rankin <Rankida@gmail.com>
2012-06-10 12:53:33 +0100

    Finished 10.4. Can now delete microposts.

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
-  before_filter :signed_in_user
+  before_filter :signed_in_user, only: [:create, :destroy]
+  before_filter :correct_user,   only: :destroy
 
   def create
     @micropost = current_user.microposts.build(params[:micropost])
@@ -13,5 +14,16 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    redirect_to root_path
   end
+
+  private
+    def correct_user
+      @micropost = current_user.microposts.find_by_id(params[:id])
+      if @micropost.nil?
+        flash[:error] = "Unable to delete post."
+        redirect_to root_path
+      end
+    end
 end
